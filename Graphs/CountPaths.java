17fab808875f6b224911e71132fac3d1f20df299
KumareshRamachandran <rkitzrk@gmail.com>
2024-06-03 15:27:14 +0530

    "Null"

--- Graphs/CountPaths.java
+++ Graphs/CountPaths.java
@@ -1,12 +1,9 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class CountPaths {
     static class Pair{
-        int first, second;
-        public Pair(int first, int second) {
+        long first, second;
+        public Pair(long first, long second) {
             this.first = first;
             this.second = second;
         }
@@ -20,27 +17,27 @@ public class CountPaths {
             adj.get(road[0]).add(new Pair(road[1], road[2]));
             adj.get(road[1]).add(new Pair(road[0], road[2]));
         }
-        PriorityQueue<Pair> pq = new PriorityQueue<>();
-        int[] dist = new int[n];
-        int[] ways = new int[n];
-        Arrays.fill(dist, (int)(1e9));
+        PriorityQueue<Pair> pq = new PriorityQueue<>(Comparator.comparingLong(x->x.first));
+        long[] dist = new long[n];
+        long[] ways = new long[n];
+        Arrays.fill(dist, Long.MAX_VALUE);
         dist[0] = 0; ways[0]=1;
         pq.add(new Pair(0,0));
         int mod = (int) (1e9+7);
         while(!pq.isEmpty()){
-            int dis = pq.peek().first, node = pq.peek().second;
+            long dis = pq.peek().first, node = pq.peek().second;
             pq.remove();
-            for(Pair it: adj.get(node)){
-                int adjNode = it.first, wt = it.second;
-                if(dis+wt<dist[adjNode]){
-                    dist[adjNode] = dis + wt;
+            for(Pair it: adj.get((int) node)){
+                long adjNode = it.first, wt = it.second;
+                if(dis+wt<dist[(int)adjNode]){
+                    dist[(int)adjNode] = dis + wt;
                     pq.add(new Pair(dis+wt, adjNode));
-                    ways[adjNode] = ways[node];
-                }else if(dis+wt==dist[adjNode]){
-                    ways[adjNode] = (ways[adjNode]+ways[node])%mod;
+                    ways[(int)adjNode] = ways[(int)node];
+                }else if(dis+wt==dist[(int)adjNode]){
+                    ways[(int)adjNode] = (ways[(int)adjNode]+ways[(int)node])%mod;
                 }
             }
         }
-        return ways[n-1]%mod;
+        return (int) ways[n-1]%mod;
     }
 }
