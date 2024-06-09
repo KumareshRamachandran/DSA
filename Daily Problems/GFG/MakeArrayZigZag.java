cf048534e2f0e13d6203ecc6782d4545e5105f07
KumareshRamachandran <rkitzrk@gmail.com>
2024-06-09 21:08:26 +0530

    "Add"

--- Daily Problems/GFG/MakeArrayZigZag.java
+++ Daily Problems/GFG/MakeArrayZigZag.java
@@ -8,7 +8,7 @@ public class MakeArrayZigZag {
     }
     public static void zigZag(int n, int[] arr) {
         for(int i=0; i<n-1; i++){
-            if(i%2==0){
+            if(i%2==0) {
                 if(arr[i]>arr[i+1]) swap(i, i+1, arr);
             }else{
                 if(arr[i]<arr[i+1]) swap(i, i+1, arr);
